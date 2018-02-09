da6cabffc60ae8ce119c9b149e6dd621213af7da
mindness <docteura@hotmail.fr>
2018-02-09 14:48:25 +0100

    Init

---
+++ RogueLike 2D/Assets/Scripts/MenuController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour {
+
+    public void PlayGame()
+    {
+
+        SceneManager.LoadScene("Main");
+        
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
